73bfc0ce4c50aa6e2b6ee10d0a6ffdad5cd2517f
Hayk Ghazaryan <hayk.ghazaryanc@gmail.com>
2023-11-08 23:11:09 +0400

    feat(types) add files types

---
+++ src/types/modules.d.ts
@@ -0,0 +1,47 @@
+declare module "*.icon.svg" {
+  import * as React from "react";
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+declare module "*.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.sass" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.styl" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.less" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.md" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.json" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.otf";
+declare module "*.ttf";
+declare module "*.eot";
+declare module "*.font.svg";
